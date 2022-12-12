da2c1d376aea016577e0213642c75004425780c9
Petra <petra.tankovic@gmail.com>
2022-12-12 10:46:59 +0100

    Define Day class specifics

--- Core/Day.cs
+++ Core/Day.cs
@@ -1,18 +1,18 @@
 using System;
 
-namespace AdventOfCode.Core
-{
-    public abstract class Day
-    {
-        int year;
-        int day;
-        string title;
+namespace AdventOfCode.Core {
+    public abstract class Day {
+        public int day {get; protected set; }
+        public int year {get; protected set; }
+        protected string title {get; set; }
 
+        protected abstract string solvePartOne();
+        protected abstract string solvePartTwo();
+        
         public void getResults() {
-            Console.WriteLine("I am a result.");
-        }
-
-        public Day() {
+            Console.WriteLine( "--- Day " + this.day + ": " + this.title + " ---");
+            Console.WriteLine("Part 1: " + solvePartOne());
+            Console.WriteLine("Part 2: " + solvePartTwo());
         }
     }
 }
