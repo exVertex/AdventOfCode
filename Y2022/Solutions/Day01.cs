eb54ea77481a48017931ad312982b99deb5894fc
Petra <petra.tankovic@gmail.com>
2022-12-12 10:48:49 +0100

    Update Day01.cs

--- Y2022/Solutions/Day01.cs
+++ Y2022/Solutions/Day01.cs
@@ -1,12 +1,22 @@
 using AdventOfCode.Core;
+using System;
 
 namespace AdventOfCode.Y2022.Solutions
 {
     public class Day01 : Day {
-        
 
-        public Day01(int year = 2022, int day = 1) {
-            
+        public Day01() {
+            day = 1;
+            year = 2022;
+            title = "Calorie counting";
+        }
+
+        protected override string solvePartOne() {
+            throw new NotImplementedException();
+        }
+
+        protected override string solvePartTwo() {
+            throw new NotImplementedException();
         }
     }
 }
