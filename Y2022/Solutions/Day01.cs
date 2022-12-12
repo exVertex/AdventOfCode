4ad6cd8df66f66cacc4eb84d0cb294f32e360882
Petra <petra.tankovic@gmail.com>
2022-12-12 15:15:29 +0100

    Add part one solution

--- Y2022/Solutions/Day01.cs
+++ Y2022/Solutions/Day01.cs
@@ -1,26 +1,45 @@
 using AdventOfCode.Core;
+using System.Collections.Generic;
+using System.Linq;
 using System;
 
 namespace AdventOfCode.Y2022.Solutions
 {
     public class Day01 : Day {
 
+        protected override dynamic preparePuzzleInput() => Utils.getArrayInput("./Y2022/PuzzleInputs/day01.txt");
+        protected override dynamic solveMain(dynamic puzzleInput) {
+            int elf = 0;
+            List<int> elfCalorieStorage = new List<int> {};
+
+            foreach (var line in puzzleInput) {
+                if (line != "") {
+                    elf += int.Parse(line);
+                } else {
+                    elfCalorieStorage.Add(elf);
+                    elf = 0;
+                }
+            };
+            elfCalorieStorage.Sort();
+
+            return elfCalorieStorage;
+        }
+
+        protected override string getPartOne(dynamic blob) {
+            
+            List<int> elfStorage = (List<int>)blob;
+            return elfStorage.Last().ToString();
+        }
+
+        protected override string getPartTwo(dynamic preparedPuzzleInput) {
+
+            return "";
+        }
+
         public Day01() {
             day = 1;
             year = 2022;
             title = "Calorie counting";
-        }
-
-        protected override dynamic preparePuzzleInput() {
-            return Utils.getArrayInput("./Y2022/PuzzleInputs/day01.txt");
-        }
-
-        protected override string solvePartOne(dynamic puzzleInput) {
-            return "";
-        }
-
-        protected override string solvePartTwo(dynamic puzzleInput) {
-            return "";
         }
     }
 }
