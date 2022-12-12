338fbb1c7642be43239f56a3cc2d4d290476f7b5
Petra <petra.tankovic@gmail.com>
2022-12-12 11:24:43 +0100

    Add first iteration UI

--- Program.cs
+++ Program.cs
@@ -1,21 +1,29 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Core;
-using AdventOfCode.Y2022.Solutions;
 
 namespace AdventOfCode {
+    
     class Program {
         static void Main(string[] args) {
 
-            var days = new List<Day> {
-                new Day01()
-            };
+            List<Day> days = Services.createDaysSolutions();
 
-            days[0].getResults();
-
-            foreach (var day in days) {
-                day.getResults();
+            // in case of no arguments, it will get the results of the last solution added
+            if (args.Length == 0) {
+                days.LastOrDefault().getResults();
             }
+            // if the format is yyyy dd, the program will return the results of that day
+            else if (args.Length == 2) {
+                foreach(Day day in days) {
+                    if (day.year == int.Parse(args[0]) && day.day == int.Parse(args[1])) {
+                        day.getResults();
+                    }
+                }
+            } else {
+                Services.printInstructions();
+            }
+            
         }    
     }
 }
