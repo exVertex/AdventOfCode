1160fd151d315aae5f33a10b063c30f95ac712d8
Petra <petra.tankovic@gmail.com>
2022-12-13 14:02:30 +0100

    Unify preparePuzzleInput method

--- Core/Day.cs
+++ Core/Day.cs
@@ -5,14 +5,15 @@ namespace AdventOfCode.Core {
         public int day {get; protected set; }
         public int year {get; protected set; }
         protected string title {get; set; }
-
-        protected abstract dynamic preparePuzzleInput();
+        
         protected abstract dynamic solveMain(dynamic puzzleInput);
         protected abstract int getPartOne(dynamic key);
         protected abstract int getPartTwo(dynamic key);
+
+        protected dynamic preparePuzzleInput(string pathToInput) => Utils.getInput(pathToInput);
         
         public void getResults() {
-            var key = solveMain(preparePuzzleInput());
+            var key = solveMain(preparePuzzleInput(string.Format("./Y{0}/PuzzleInputs/day{1}.txt", this.year, this.day.ToString("00"))));
             Console.WriteLine( "--- Day " + this.day + ": " + this.title + " ---");
             Console.WriteLine("Part 1: " + getPartOne(key));
             Console.WriteLine("Part 2: " + getPartTwo(key));
