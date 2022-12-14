8f904985e5a744157c22b73518ff2a3e4a19434b
Petra <petra.tankovic@gmail.com>
2022-12-14 09:24:00 +0100

    Add solution to part two

--- Y2022/Solutions/Day03.cs
+++ Y2022/Solutions/Day03.cs
@@ -8,19 +8,26 @@ namespace AdventOfCode.Y2022.Solutions {
 
         protected int getPriorityValue(int item) => (item > 91) ? item - 96 : item - 38;    // get value of the priority through char ascii values
         
-        protected override dynamic solveMain(string[] puzzleInput) {
-            List<char> commonItems = new List<char>();
-            
-            Array.ForEach(puzzleInput, rucksack => commonItems.Add( // fill commonItems with letters contained in both substrings
+        protected override dynamic solveMain(string[] puzzleInput) => puzzleInput;  // no common main part to solve
+
+        protected override int getPartOne(dynamic key) {
+            List<char> commonItems = new List<char>();  // list to store items common in both compartments
+
+            Array.ForEach((string[])key, rucksack => commonItems.Add( // fill commonItems with letters contained in both substrings
                 rucksack.Substring(0, rucksack.Length/2).Intersect(rucksack.Substring(rucksack.Length/2)).FirstOrDefault()));
 
-            return commonItems;
+            return commonItems.Sum(item => getPriorityValue((int)item));   // return sum of all priorities
         }
 
-        protected override int getPartOne(dynamic key) => ((List<char>)key).Sum(item => getPriorityValue((int)item));   // return sum of all priorities
-
         protected override int getPartTwo(dynamic key) {
-            return 0;
+            List<string> rucksacks = new List<string>(key); // store puzzle input in list of strings
+            List<char> commonItems = new List<char>(); // list to store items common in both compartments
+
+            for (int i = 0; i < rucksacks.Count; i += 3) {
+                commonItems.Add(rucksacks[i].Intersect(rucksacks[i+1].Intersect(rucksacks[i+2])).FirstOrDefault());
+            }
+
+            return commonItems.Sum(item => getPriorityValue((int)item));
         }
 
         public Day03() {
