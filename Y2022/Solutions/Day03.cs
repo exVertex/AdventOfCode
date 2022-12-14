122383a326420b2299fa0202a0cf64076ec9b100
Petra <petra.tankovic@gmail.com>
2022-12-14 08:16:06 +0100

    Add solution to part one

--- Y2022/Solutions/Day03.cs
+++ Y2022/Solutions/Day03.cs
@@ -1,17 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Core;
 
 namespace AdventOfCode.Y2022.Solutions {
     public class Day03 : Day {
-        protected override dynamic solveMain(dynamic puzzleInput) {
-            throw new System.NotImplementedException();
+
+        protected int getPriorityValue(int item) => (item > 91) ? item - 96 : item - 38;    // get value of the priority through char ascii values
+        
+        protected override dynamic solveMain(string[] puzzleInput) {
+            List<char> commonItems = new List<char>();
+            
+            Array.ForEach(puzzleInput, rucksack => commonItems.Add( // fill commonItems with letters contained in both substrings
+                rucksack.Substring(0, rucksack.Length/2).Intersect(rucksack.Substring(rucksack.Length/2)).FirstOrDefault()));
+
+            return commonItems;
         }
 
-        protected override int getPartOne(dynamic key) {
-            throw new System.NotImplementedException();
-        }
+        protected override int getPartOne(dynamic key) => ((List<char>)key).Sum(item => getPriorityValue((int)item));   // return sum of all priorities
 
         protected override int getPartTwo(dynamic key) {
-            throw new System.NotImplementedException();
+            return 0;
         }
 
         public Day03() {
