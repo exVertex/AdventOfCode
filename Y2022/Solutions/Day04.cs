d7f7968793c1b9bde884cc5b29e258b75b611729
Petra <petra.tankovic@gmail.com>
2022-12-14 14:28:11 +0100

    Add solution to part two and format solveMain

--- Y2022/Solutions/Day04.cs
+++ Y2022/Solutions/Day04.cs
@@ -1,36 +1,38 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using AdventOfCode.Core;
 
 namespace AdventOfCode.Y2022.Solutions {
     public class Day04 : Day {
 
-        private int checkIfOverlapping (int[] top, int[] bottom) => (top[1] >= bottom[1] && bottom[0] >= top[0]) ? 1 : 0;
+        private int checkIfContains (int[] top, int[] bottom) => (top[1] >= bottom[1] && bottom[0] >= top[0]) ? 1 : 0; // checks if one task range contains the other
+        private int checkIfOverlaps (int[] top, int[] bottom) => (bottom[1] >= top[0]) ? 1 : 0; // checks if task ranges overlap
 
         protected override dynamic solveMain(string[] puzzleInput) {
-            int overlappingAssigmentPairs = 0;
+            List<int[][]> elfGroups = new List<int[][]>();
 
-            foreach(var line in puzzleInput) {
-                string[] elfGroup = line.Split(",");
-                string[] leftPart = elfGroup[0].Split("-");
-                string[] rightPart = elfGroup[1].Split("-");
-                int[] leftElf = new int[] {int.Parse(leftPart[0]), int.Parse(leftPart[1])};
-                int[] rightElf = new int[] {int.Parse(rightPart[0]), int.Parse(rightPart[1])};
+            foreach (string line in puzzleInput) {
+                string[] elfGroup = line.Split(",");    // split the line into left and rigt assignment parts
+                string[] leftElf = elfGroup[0].Split("-");  // get task range of the first elf
+                string[] rightElf = elfGroup[1].Split("-"); // get task range of the second elf
 
-                overlappingAssigmentPairs += (leftElf[1] > rightElf[1] || (leftElf[1] == rightElf[1] && leftElf[0] < rightElf[0])) ?
-                checkIfOverlapping(leftElf, rightElf) : checkIfOverlapping(rightElf, leftElf);
+                elfGroups.Add(new int[] [] {
+                    new int[] {int.Parse(leftElf[0]), int.Parse(leftElf[1])},
+                    new int[] {int.Parse(rightElf[0]), int.Parse(rightElf[1])}
+                });
             }
 
-            return overlappingAssigmentPairs;
+            return elfGroups;   // returns puzzle input in the form of list of jagged arrays
         }
 
-        protected override int getPartOne(dynamic key) {
-            return key;
-        }
-
-        protected override int getPartTwo(dynamic key) {
-
-            
-            return 0;
-        }
+        protected override int getPartOne(dynamic key) => (new List<int[][]>(key)).Sum(
+            elfGroup => (elfGroup[0][1] > elfGroup[1][1] || (elfGroup[0][1] == elfGroup[1][1] && elfGroup[0][0] < elfGroup[1][0])) ?
+            checkIfContains(elfGroup[0], elfGroup[1]) : checkIfContains(elfGroup[1], elfGroup[0]));
+        
+        protected override int getPartTwo(dynamic key) => (new List<int[][]>(key)).Sum(
+            elfGroup => (elfGroup[0][1] > elfGroup[1][1] || (elfGroup[0][1] == elfGroup[1][1] && elfGroup[0][0] < elfGroup[1][0])) ?
+            checkIfOverlaps(elfGroup[0], elfGroup[1]) : checkIfOverlaps(elfGroup[1], elfGroup[0]));
 
         public Day04() {
             day = 4;
