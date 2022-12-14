a3215a7c3892bcec62145961e0973d6d191f0d5e
PETRA TANKOVIĆ <pt6346@student.uni-lj.si>
2022-12-14 16:29:53 +0100

    Add Day extention

--- Y2021/Solutions/Day01.cs
+++ Y2021/Solutions/Day01.cs
@@ -2,10 +2,7 @@ using AdventOfCode.Core;
 using System;
 
 namespace AdventOfCode.Y2021.Solutions {
-    class Day01 {
-        /// <summary> Calculates the number of times a depth (puzzle input) measurement increases from the previous measurement. </summary>
-        /// <param name="input"> Puzzle input. </param>
-        /// <returns> Sum of the increases. </returns>
+    class Day01 : Day {
         private static int calculcateMeasuermentIncreases(int[] input) {
             int prevNumber = 0;
             int increases = -1;
@@ -17,9 +14,6 @@ namespace AdventOfCode.Y2021.Solutions {
             return increases;
         }
 
-        /// <summary> Calculates the number of times the sum of measurements in this sliding window increases. </summary>
-        /// <param name="input"> Puzzle input. </param>
-        /// <returns> Sum of the three-measurements increases. </returns>
         private static int calculcateThreeSumMeasuermentIncreases(int[] input) {
             int increases = -1;
             int sumA, prevSum, i;
@@ -33,13 +27,16 @@ namespace AdventOfCode.Y2021.Solutions {
             }
             return increases;
         }
-        public static void getResults(){
-            var puzzleInput = Utils.parsePuzzleInputToIntegerArray("./Y2021/PuzzleInputs/day01.txt");
-
-            Console.WriteLine("~~~~~~~~~~~~~ Day 1 ~~~~~~~~~~~~~");
-            Console.WriteLine("Part 1: " + calculcateMeasuermentIncreases(puzzleInput));
-            Console.WriteLine("Part 2: " + calculcateThreeSumMeasuermentIncreases(puzzleInput));                        
+        protected override dynamic solveMain(string[] puzzleInput) {
+            throw new NotImplementedException();
         }
 
+        protected override int getPartOne(dynamic key) {
+            throw new NotImplementedException();
+        }
+
+        protected override int getPartTwo(dynamic key) {
+            throw new NotImplementedException();
+        }
     }  
 }   
