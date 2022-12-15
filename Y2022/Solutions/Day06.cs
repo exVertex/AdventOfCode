3c236eda9e052d8da6c32a8dd4bfba14e7665029
Petra <petra.tankovic@gmail.com>
2022-12-15 13:32:51 +0100

    Add day 6 solution

--- Y2022/Solutions/Day06.cs
+++ Y2022/Solutions/Day06.cs
@@ -1,24 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Core;
 
 namespace AdventOfCode.Y2022.Solutions {
     public class Day06 : Day {
 
-        protected override dynamic solveMain(string[] puzzleInput) {
-            throw new System.NotImplementedException();
+        private int getMarkerPosition(string datastream, int markerSize) {
+
+            for (int i = 0; i < datastream.Length - markerSize; i++) {
+                var possibleMarker = new List<char>(datastream.Substring(i, markerSize));   // store characters for validation into a char array 
+                if (possibleMarker.Distinct().Count() == possibleMarker.Count()) return i + markerSize;  // check if characters in the list are unique
+            }
+            
+            return datastream.Length;   // safety net
         }
 
-        protected override int getPartOne(dynamic key) {
-            throw new System.NotImplementedException();
-        }
+        protected override dynamic solveMain(string[] puzzleInput) => puzzleInput[0];   //return a string instead an array of strings
 
-        protected override int getPartTwo(dynamic key) {
-            throw new System.NotImplementedException();
-        }
+        protected override int getPartOne(dynamic key) => getMarkerPosition(key, 4);
+
+        protected override int getPartTwo(dynamic key) => getMarkerPosition(key, 14);
 
         public Day06() {
             day = 6;
             year = 2022;
-            title = "";
+            title = "Tuning Trouble";
         }
     }
 }
