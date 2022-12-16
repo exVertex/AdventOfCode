4fd4c38d6858908b2f18dd908401a70de0d90d97
Petra <petra.tankovic@gmail.com>
2022-12-16 11:32:08 +0100

    Add puzzleInput parsing to Day08

--- Y2022/Solutions/Day08.cs
+++ Y2022/Solutions/Day08.cs
@@ -3,21 +3,27 @@ using AdventOfCode.Core;
 namespace AdventOfCode.Y2022.Solutions {
     public class Day08: Day {
         protected override dynamic prepareCommon(string[] puzzleInput) {
-            throw new System.NotImplementedException();
+            var treePlantation = new int[puzzleInput.Length][]; // new jagged int[] to store tree heights
+
+            for (int i = 0; i < puzzleInput.Length; i++) {  // fill treePlantation array with array of converted integers
+                treePlantation[i] = Utils.convertToIntArray(puzzleInput[i].ToCharArray());
+            }
+
+            return treePlantation;
         }
 
         protected override int getPartOne(dynamic key) {
-            throw new System.NotImplementedException();
+            return 0;
         }
 
         protected override int getPartTwo(dynamic key) {
-            throw new System.NotImplementedException();
+            return 0;
         }
 
         public Day08() {
             day = 8;
             year = 2022;
-            title = "Treetop Tree House ";
+            title = "Treetop Tree House";
         }
     }
 }
