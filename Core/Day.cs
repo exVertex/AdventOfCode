ebd3010cc7cc7463e700ebf740df5594a5665f3d
Petra <petra.tankovic@gmail.com>
2023-12-01 13:26:44 +0100

    Add watch to Day class

--- Core/Day.cs
+++ Core/Day.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Diagnostics;
 
 namespace AdventOfCode.Core {
     public abstract class Day {
-        public int day {get; protected set; }
-        public int year {get; protected set; }
-        protected string title {get; set; }
+        public int day { get; protected set; }
+        public int year { get; protected set; }
+        protected string title { get; set; }
         
         protected virtual dynamic prepareCommon(string[] puzzleInput) => puzzleInput;
         protected abstract int getPartOne(dynamic key);
@@ -14,9 +15,20 @@ namespace AdventOfCode.Core {
         
         public void getResults() {
             var key = prepareCommon(preparePuzzleInput(string.Format("./Y{0}/PuzzleInputs/day{1}.txt", this.year, this.day.ToString("00"))));
+
+            var watch = new Stopwatch();
+
             Console.WriteLine( "--- Day " + this.day + ": " + this.title + " ---");
-            Console.WriteLine("Part 1: " + getPartOne(key));
-            Console.WriteLine("Part 2: " + getPartTwo(key));
+            
+            watch.Start();
+            var partOne = getPartOne(key);
+            watch.Stop();
+            Console.WriteLine("Part 1: " + partOne + " (" + watch.ElapsedMilliseconds + " ms)");
+            
+            watch.Start();
+            var partTwo = getPartTwo(key);
+            watch.Stop();
+            Console.WriteLine("Part 2: " + partTwo + " (" + watch.ElapsedMilliseconds + " ms)");
         }
     }
 }
