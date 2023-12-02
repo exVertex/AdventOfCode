0f0d352eecfd7e11853bf3bcec2b207803e753a4
Petra <petra.tankovic@gmail.com>
2023-12-02 17:09:42 +0100

    Fix solution for day 1, 20222

--- Y2022/Solutions/Day01.cs
+++ Y2022/Solutions/Day01.cs
@@ -4,25 +4,24 @@ using System.Linq;
 
 namespace AdventOfCode.Y2022.Solutions {
     public class Day01 : Day {
-       
         protected override dynamic PrepareCommon(string[] puzzleInput) {
             var elfCalorieStorage = new List<int> {};
+            int elfCalories = 0;
 
-            foreach (var line in puzzleInput) {
-                int elf = 0;
-                if (string.IsNullOrWhiteSpace(line)) {
-                    elf += int.Parse(line);
+            for (int i = 0; i < puzzleInput.Length; i++) {
+                if(!string.IsNullOrWhiteSpace(puzzleInput[i])) {
+                    elfCalories += int.Parse(puzzleInput[i]);
                 } else {
-                    elfCalorieStorage.Add(elf);
+                    elfCalorieStorage.Add(elfCalories);
+                    elfCalories = 0;
                 }
-            };
-            
+                if (i == puzzleInput.Length -1) elfCalorieStorage.Add(elfCalories);
+            }
+
             return elfCalorieStorage.OrderByDescending(x => x).ToList();
         }
-
         protected override int GetPartOne(dynamic key) => ((List<int>)key).First();
         protected override int GetPartTwo(dynamic key) => key[0] + key[1] + key[2];
-
         public Day01() {
             day = 1;
             year = 2022;
